b2f440eff7d87081973934dc02cacb70ba7d530c
Pelu Adejugba <pelu.adejugba@gmail.com>
2025-01-07 16:52:11 -0800

    style the active link in the navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,8 +1,14 @@
+'use client'
+
 import React from 'react'
 import Link from 'next/link'
 import { GoBug } from "react-icons/go";
+import { usePathname } from 'next/navigation';
+import classNames from 'classnames';
 
 const Navbar = () => {
+  const currentPath = usePathname()
+
   const links = [
     { label: 'Dashboard', id: 2, href: '/' },
     { label: 'Issues', id: 3, href: '/issues' }
@@ -12,7 +18,11 @@ const Navbar = () => {
     <nav className='flex space-x-5 border-b mb-5 px-5 h-12 items-center'>
       <Link href='/'> <GoBug /></Link>
       {links.map(link => <Link
-        className='text-zinc-500 hover:text-zinc-800 transition-colors'
+        className={classNames({
+          'text-zinc-900': link.href === currentPath,
+          'text-zinc-500': link.href !== currentPath,
+          'hover:text-zinc-800': true
+        })} 
         key={link.id}
         href={link.href}
       >{link.label}</Link>)}
